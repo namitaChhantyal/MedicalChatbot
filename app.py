6a227bca7b14f34492e82daed2ebe124781906f9
namitaChhantyal <chhantyalnamita111@gmail.com>
2025-12-09 09:52:51 -0500

    app file modified

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template,jsonify,request, session
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 from langchain_pinecone import PineconeVectorStore
